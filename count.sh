4aeb31b7e512209a3a1e1ca2d9a6d3ce68086b8b
Corentin Ferry <cocodidouf@gmail.com>
2016-03-21 22:41:44 +0100

    isn't a global counter better ?

--- count.sh
+++ count.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+total=0
+
 for f in questions/*
 do
 printf "%-.50s %s\n" "$f........................................................................." $(grep -c '\*\*' $f)
+total=$((total+$(grep -c '\*\*' $f)))
 
 done
+
+echo "Total : $total questions"
